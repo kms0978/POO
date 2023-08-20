bab61dda926a7cbcab907d576399cb3561208bf3
Alumno <k.millaldeo01@ufromail.cl>
2023-08-20 18:59:42 -0400

    nothing

--- src/PrograOO.java
+++ src/PrograOO.java
@@ -2,5 +2,7 @@ public class PrograOO {
     public static void main(String[] args){
         System.out.println("¡Hola, mundo!");
         System.out.println("2023");
+        int n = 1234567890;
+        System.out.println(n);
     }
 }
